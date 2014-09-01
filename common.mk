f95c13c3bb158ad5042d31c0439f8c7348011d6b
maxwen <max.weninger@gmail.com>
2014-09-01 17:36:14 +0200

    audio: add qcom effect config and the other qcom libs
    Change-Id: Ia161bfc54ba369e024607d07f3fc3a7ea43d19b3

--- common.mk
+++ common.mk
@@ -56,6 +56,7 @@ PRODUCT_COPY_FILES += \
 # Audio config files
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(COMMON_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(COMMON_PATH)/mixer_paths.xml:/system/etc/mixer_paths.xml
 
 # MSM IPC Router security configuration
@@ -106,7 +107,9 @@ PRODUCT_PACKAGES += \
     camera-wrapper.msm8974 \
     libaudio-resampler \
     audiod \
-    libqcompostprocbundle
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing
 
 PRODUCT_PACKAGES += \
     libmm-omxcore \
