d1305f0d17abb2d8f4fdde340f91484bff7ade1b
maxwen <max.weninger@gmail.com>
2014-11-15 18:06:29 +0100

    oppo-common: bringup for 5.0
    depends on https://gerrit.omnirom.org/#/c/10483/
    
    Change-Id: I7797d6a74f894b76d302fea4bc8414e24df5d305

--- common.mk
+++ common.mk
@@ -32,8 +32,8 @@ PRODUCT_PACKAGES += \
     librs_jni
 
 # Omni Packages
-PRODUCT_PACKAGES += \
-    OmniTorch
+#PRODUCT_PACKAGES += \
+#    OmniTorch
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
@@ -51,7 +51,10 @@ PRODUCT_COPY_FILES += \
 # Media config files
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
-    $(COMMON_PATH)/media_profiles.xml:system/etc/media_profiles.xml
+    $(COMMON_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Audio config files
 PRODUCT_COPY_FILES += \
@@ -110,8 +113,7 @@ PRODUCT_PACKAGES += \
     audiod \
     libqcompostprocbundle \
     libqcomvisualizer \
-    libqcomvoiceprocessing \
-    libdashplayer
+    libqcomvoiceprocessing
 
 PRODUCT_PACKAGES += \
     libmm-omxcore \
@@ -119,17 +121,22 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libOmxCore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
-    libOmxEvrcEnc \
-    libOmxQcelp13Enc \
     libstagefrighthw \
     libc2dcolorconvert
+
+#    libOmxAacEnc \
+#    libOmxAmrEnc \
+#    libOmxEvrcEnc \
+#    libOmxQcelp13Enc \
 
 # wifi
 PRODUCT_PACKAGES += \
     mac-update \
-    wcnss_service
+    wcnss_service \
+    libwpa_client \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
@@ -218,7 +225,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     use.voice.path.for.pcm.voip=true \
     av.offload.enable=true \
     av.streaming.offload.enable=true \
-    audio.offload.pcm.enable=true
+    audio.offload.pcm.enable=true \
+    persist.sys.media.use-awesome=true
 
 # QCOM
 PRODUCT_PROPERTY_OVERRIDES += \
