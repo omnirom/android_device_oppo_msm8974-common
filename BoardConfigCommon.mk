1d46d195c47304573424877a2f7026058deb3e30
maxwen <max.weninger@gmail.com>
2015-03-29 00:04:29 +0100

    selinux: switch to enforcing
    In security we trust :)
    
    Change-Id: Ia2984c740951b41c1494b8c4bddc7acc63ffcf8b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,7 +21,7 @@ TARGET_CPU_SMP := true
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
 
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.selinux=permissive androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
