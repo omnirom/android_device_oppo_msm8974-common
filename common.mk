618b376733eeaaf3caba010a5bbacf37d9c291ab
maxwen <max.weninger@gmail.com>
2015-10-12 01:44:22 +0200

    msm8974-common : updates to match oppo blobs (2.1.4)
    camera parameter and wrapper
    
    Change-Id: Ife3b678bee8428182cc5312fb97eddc009e32e25

--- common.mk
+++ common.mk
@@ -180,6 +180,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Ril
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
+    rild.libargs=-d /dev/smd0 \
+    persist.rild.nitz_plmn= \
+    persist.rild.nitz_long_ons_0= \
+    persist.rild.nitz_long_ons_1= \
+    persist.rild.nitz_long_ons_2= \
+    persist.rild.nitz_long_ons_3= \
+    persist.rild.nitz_short_ons_0= \
+    persist.rild.nitz_short_ons_1= \
+    persist.rild.nitz_short_ons_2= \
+    persist.rild.nitz_short_ons_3= \
     ril.subscription.types=NV,RUIM
 
 # Cell Broadcasts
@@ -236,12 +246,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # QC Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=/vendor/lib/libqc-opt.so
+    ro.vendor.extension_library=/vendor/lib/libqti-perfd-client.so
 
 # gps
-#system prop for switching gps driver to qmi
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.gps.qmienabled=true
+    persist.gps.qc_nlp_in_use=1 \
+    ro.gps.agps_provider=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
