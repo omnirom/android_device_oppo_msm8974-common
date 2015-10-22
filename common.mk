f7bec0ab7e525070eef7760649d993cbc9058fb2
maxwen <max.weninger@gmail.com>
2015-10-22 03:30:48 +0200

    common: fix build for 6.0
    changes for initial bringup
    
    Change-Id: Ia0c6b5f7c6c1badeec8cb44765351bdc3e7a10c3

--- common.mk
+++ common.mk
@@ -122,7 +122,9 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libOmxCore \
     libstagefrighthw \
-    libc2dcolorconvert
+    libc2dcolorconvert \
+    libxml2 \
+    libboringssl-compat
 
 #    libOmxAacEnc \
 #    libOmxAmrEnc \
@@ -220,7 +222,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence \
     persist.audio.fluence.voicecall=true \
-    af.resampler.quality=4 \
     audio.offload.buffer.size.kb=32 \
     audio.offload.gapless.enabled=true \
     use.voice.path.for.pcm.voip=true \
