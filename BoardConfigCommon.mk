050871a769cc51df19efa5e4a44d5178758093ce
maxwen <max.weninger@windriver.com>
2015-11-02 12:29:50 +0100

    wifi: fix MAC generation
    Change-Id: Ia32bbe18e468bfca8b6e10d63a7c0bd9b28ddbd6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -90,6 +90,9 @@ WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
 WIFI_DRIVER_MODULE_NAME          := "wlan"
+TARGET_USES_QCOM_WCNSS_QMI       := true
+TARGET_PROVIDES_WCNSS_QMI        := true
+TARGET_USES_WCNSS_MAC_ADDR_REV   := true
 
 BOARD_EGL_CFG := $(COMMON_PATH)/configs/egl.cfg
 
