821f254c4baf56a5fa8bc34d5838cc4d720ba62f
maxwen <max.weninger@gmail.com>
2016-03-02 00:29:55 +0100

    Find7Parts: no dexpreopt - for heavens sake
    Change-Id: I09925cf9732213c2d407da125e916f1fe78e98c5

--- Find7Parts/Android.mk
+++ Find7Parts/Android.mk
@@ -7,9 +7,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := Find7Parts
 LOCAL_CERTIFICATE := platform
-
-# Required so that symbols used by the jni library doesn't get stripped out
-LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_DEX_PREOPT := false
 
 include $(BUILD_PACKAGE)
 
