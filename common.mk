8c0075035f545cb4d775b63a00ac74a48c42b901
Philipp Vogel <philippwolf86.pw@gmail.com>
2016-11-15 16:15:08 +0100

    common: Remove obsolete dhcpcd init entries
    Change-Id: Ib4f9aba5e10cdd7ca00b776d0aaa7a5c934fb2a9

--- common.mk
+++ common.mk
@@ -139,8 +139,7 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \
     wpa_supplicant \
-    wpa_supplicant.conf \
-    dhcpcd.conf
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
@@ -263,8 +262,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/sensor/sap.conf:system/etc/sap.conf
 
 # Telephony extension
-PRODUCT_PACKAGES += telephony-ext
-PRODUCT_BOOT_JARS += telephony-ext
+#PRODUCT_PACKAGES += telephony-ext
+#PRODUCT_BOOT_JARS += telephony-ext
 
 # Inherit from proprietary blobs
 $(call inherit-product, vendor/oppo/msm8974-common/msm8974-common-vendor.mk)
