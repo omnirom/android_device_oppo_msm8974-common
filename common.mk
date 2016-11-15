5efa57b428c60b7534e1a63410fda76c057d310f
Philipp Vogel <philippwolf86.pw@gmail.com>
2016-11-15 16:15:08 +0100

    common: Remove obsolete dhcpcd init entries
    Change-Id: Ib4f9aba5e10cdd7ca00b776d0aaa7a5c934fb2a9

--- common.mk
+++ common.mk
@@ -139,8 +139,7 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \
     wpa_supplicant \
-    wpa_supplicant.conf \
-    dhcpcd.conf
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
